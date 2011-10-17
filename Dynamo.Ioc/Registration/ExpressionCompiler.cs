ee9d622dbae66325820ecea74d7e961468fdfbac
Martin From <from@pc.dk>
2011-10-17 02:59:37 +0200

    Small cleanup of the expression compiler

--- Dynamo.Ioc/Registration/ExpressionCompiler.cs
+++ Dynamo.Ioc/Registration/ExpressionCompiler.cs
@@ -5,15 +5,10 @@ using System.Linq.Expressions;
 using System.Text;
 using Dynamo.Ioc.Index;
 
-// Ohhhh God ! This need cleaning up !
-// Create methods / Helpers for what is going on in the VisitMethodCall() to make it easier to work with.
-
 // Add Support for TryResolve (Type/Key, and <T>) - if not found just inline null, or keep TryResolve expression.
 // Turn Generic method calls into the equvilant using type directly ? - very small improvement
 
 // Add support for using cached compiled registration to speed it up instead of compiling the whole "tree" for each registration ?
-
-// namespace ?
 
 namespace Dynamo.Ioc.Registration
 {
@@ -52,33 +47,57 @@ namespace Dynamo.Ioc.Registration
 			return false;
 		}
 
-		private bool TryGetRegistration(Type type, object key, out IRegistration registration)
+		protected override Expression VisitUnary(UnaryExpression node)
 		{
-			return key == null ? _index.TryGet(type, out registration) : _index.TryGet(type, key, out registration);		
+			// Cleanup unessecary cast
+
+			var exp = base.VisitUnary(node);
+
+			// if it has been changed
+			if (exp != node && exp.NodeType == ExpressionType.Convert)
+			{
+				var convertExp = (UnaryExpression)exp;
+
+				// Check that what is inside the operand is of the type expected
+				if (convertExp.Type.IsAssignableFrom(convertExp.Operand.Type))
+				{
+					// Remove the convert by returning the operand
+					return convertExp.Operand;
+				}
+			}
+
+			return exp;
 		}
 
-		private Expression HandleMethodCall(Type type, object key = null)
+		private IRegistration GetRegistration(Type type, object key)
 		{
-			// Split up this method ?
-
+			if (type == null)
+				throw new ArgumentNullException("type");
+			
 			IRegistration registration;
 
-			if (!TryGetRegistration(type, key, out registration))
+			if (!(key == null ? _index.TryGet(type, out registration) : _index.TryGet(type, key, out registration)))
 			{
 				// Not able to find registration referenced.
 				// Could choose to ignore it and let it fail later when trying to Resolve if the type/key havent been added to the index inbetween then.
 
 				var exceptionDescription = "Cannot Compile Registration. The registration have a reference to Type: " + type.Name;
 				if (key != null)
-					exceptionDescription += " with Key: " + key;
+					exceptionDescription += " with Key: " + key;	// only if string it makes sense to use the ToString on the object ? else get the type ?
 				exceptionDescription += " which isn't registered.";
 
 				// TODO: Throw CompileException or something ?
 				throw new RegistrationException(_currentRegistration, exceptionDescription);
 			}
 
-			// Only possible to compile ExpressionRegistration's
+			return registration;
+		}
 
+		private Expression HandleMethodCall(Type type, object key = null)
+		{
+			IRegistration registration = GetRegistration(type, key);
+
+			// Only possible to compile ExpressionRegistration's
 			var expressionRegistration = registration as ExpressionRegistration;
 			if (expressionRegistration != null)
 			{
@@ -106,30 +125,8 @@ namespace Dynamo.Ioc.Registration
 			return null;
 		}
 
-		protected override Expression VisitUnary(UnaryExpression node)
+		private bool IsMethodOnParameter(MethodCallExpression expression, Type instanceType)
 		{
-			var exp = base.VisitUnary(node);
-
-			// if it has been changed
-			if (exp != node && exp.NodeType == ExpressionType.Convert)
-			{
-				var convertExp = (UnaryExpression)exp;
-
-				// Check that what is inside the operand is of the type expected
-				if (convertExp.Type.IsAssignableFrom(convertExp.Operand.Type))
-				{
-					// Remove the convert by returning the operand
-					return convertExp.Operand;
-				}
-			}
-
-			return exp;
-		}
-
-		private bool IsInstanceMethod(MethodCallExpression expression, Type instanceType)
-		{
-			// Better and more correct name ?
-
 			if (expression == null)
 				throw new ArgumentNullException("expression");
 			if (instanceType == null)
@@ -170,16 +167,15 @@ namespace Dynamo.Ioc.Registration
 
 		protected override Expression VisitMethodCall(MethodCallExpression methodExpression)
 		{
-			// TODO: Someday this nasty nesting needs to be fixed ... zzZzzzZzz
 			// Cleanup handling the parameters
 			// Support TryResolve (Type/Key, <T>/Key)
 
-			if (IsInstanceMethod(methodExpression, typeof(IResolver)))
+			if (IsMethodOnParameter(methodExpression, typeof(IResolver)))
 			{
 				// Resolve(Type) & Resovle(Type, Key)
 				if (IsMethod(methodExpression, "Resolve", false))
 				{
-					var arguments = methodExpression.Arguments;		// Arguments for the call
+					var arguments = methodExpression.Arguments;
 
 					// Check that arguments was supplied and first one is of type Type
 					if (arguments.Count > 0 && arguments[0] is ConstantExpression && arguments[0].Type == typeof(Type))
@@ -187,18 +183,17 @@ namespace Dynamo.Ioc.Registration
 						var typeArgument = (ConstantExpression)arguments[0];
 						var type = (Type)typeArgument.Value;
 
-						if (arguments.Count == 1)
+						if (arguments.Count == 1) // object Resolve(Type type)
 						{
-							// object Resolve(Type type)
-
 							return HandleMethodCall(type) ?? base.VisitMethodCall(methodExpression);
 						}
-						else if (arguments.Count == 2)	// && arguments[1] is ConstantExpression && arguments[1].Type == typeof(Object))		// TODO PROBLEM HERE IF STRING !?!?!? it contains the actuall type us is Object?
-						{
-							// object Resolve(Type type, object key)
 
+						if (arguments.Count == 2) // object Resolve(Type type, object key)	
+						{
+							// Also check - && arguments[1] is ConstantExpression && arguments[1].Type == typeof(Object)) ? Last check wont work if etc string is supplied, and checking if it is an object doesnt make sense as everything is an object?
+							
 							var keyArgument = (ConstantExpression)arguments[1];
-							var key = (Object)keyArgument.Value;
+							var key = keyArgument.Value;
 
 							return HandleMethodCall(type, key) ?? base.VisitMethodCall(methodExpression);
 						}
@@ -206,8 +201,6 @@ namespace Dynamo.Ioc.Registration
 
 					// Throw exception ?
 				}
-
-
 
 				// Resolve<>() & Resolve<>(key)
 				if (IsMethod(methodExpression, "Resolve", true))
@@ -232,18 +225,17 @@ namespace Dynamo.Ioc.Registration
 							// Need to have the expression which is being visited stored somewhere !?
 							//var methodArgument1 = (ParameterExpression)arguments[0];
 
-							if (arguments.Count == 0)
+							if (arguments.Count == 0) // T Resolve<T>()
 							{
-								// T Resolve<T>()
-
 								return HandleMethodCall(genericType) ?? base.VisitMethodCall(methodExpression);
 							}
-							else if (arguments.Count == 1) // && arguments[1] is ConstantExpression && arguments[1].Type == typeof(Object))		// TODO PROBLEM HERE ! contains the actual type and not object probably so never gets compiled !?
+
+							if (arguments.Count == 1) // T Resolve<T>(object key)
 							{
-								// T Resolve<T>(object key)
+								// && arguments[1] is ConstantExpression && arguments[1].Type == typeof(Object))		// TODO PROBLEM HERE ! contains the actual type and not object probably so never gets compiled !?
 
 								var keyArgument = (ConstantExpression)arguments[0];
-								var key = (Object)keyArgument.Value;
+								var key = keyArgument.Value;
 
 								return HandleMethodCall(genericType, key) ?? base.VisitMethodCall(methodExpression);
 							}
