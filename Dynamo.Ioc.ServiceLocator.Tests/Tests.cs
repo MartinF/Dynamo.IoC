0ae34ae1b134c5fb97a3e0d22dac4a95b1be1aa6
GeorgeR <dukecg@gmail.com>
2012-05-27 21:42:33 +1000

    Added CSL adaptor
    Not familiar with MS Test - I added one anyway but couldnt run them as
    Local.testsettings doesnt exist

---
+++ Dynamo.Ioc.ServiceLocator.Tests/Tests.cs
@@ -0,0 +1,19 @@
+﻿namespace Dynamo.Ioc.ServiceLocator.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ServiceLocator = Microsoft.Practices.ServiceLocation.ServiceLocator;
+
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void CanSetProvider()
+        {
+            var resolver = new IocContainer();
+            var locator = new DynamoServiceLocator(resolver);
+            ServiceLocator.SetLocatorProvider(() => locator);
+
+            // No way to get provider from ServiceLocator.Current to verify!!
+        }
+    }
+}
---
+++ Dynamo.Ioc.ServiceLocator/DynamoServiceLocator.cs
@@ -0,0 +1,29 @@
+﻿namespace Dynamo.Ioc.ServiceLocator
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Practices.ServiceLocation;
+
+    public class DynamoServiceLocator : ServiceLocatorImplBase
+    {
+        private readonly IResolver _resolver;
+
+        public DynamoServiceLocator(IResolver resolver)
+        {
+            _resolver = resolver;
+        }
+
+        protected override object DoGetInstance(Type serviceType, string key)
+        {
+            if (!string.IsNullOrEmpty(key))
+                return _resolver.Resolve(serviceType, key);
+
+            return _resolver.Resolve(serviceType);
+        }
+
+        protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
+        {
+            return _resolver.ResolveAll(serviceType);
+        }
+    }
+}
