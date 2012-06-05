05efdc96e6037f4e1bc1f304f8d466f4c95e7448
Martin From <from@pc.dk>
2012-06-05 05:53:50 +0200

    DependencyResolver only needs IResolver.

--- Dynamo.Ioc.Web/DynamoDependencyResolver.cs
+++ Dynamo.Ioc.Web/DynamoDependencyResolver.cs
@@ -9,31 +9,31 @@ namespace Dynamo.Ioc.Web
 {
 	public class DynamoDependencyResolver : IServiceProvider, System.Web.Mvc.IDependencyResolver, System.Web.Http.Dependencies.IDependencyResolver
 	{
-		private readonly IIocContainer _container;
+		private readonly IResolver _resolver;
 
-		public DynamoDependencyResolver(IIocContainer container)
+		public DynamoDependencyResolver(IResolver resolver)
 		{
-			if (container == null)
-				throw new ArgumentNullException("container");
-			
-			_container = container;
+			if (resolver == null)
+				throw new ArgumentNullException("resolver");
+
+			_resolver = resolver;
 		}
 
 		public object GetService(Type serviceType)
 		{
 			object obj;
-			_container.TryResolve(serviceType, out obj);
+			_resolver.TryResolve(serviceType, out obj);
 			return obj;
 		}
 
 		public IEnumerable<object> GetServices(Type serviceType)
 		{
-			return _container.TryResolveAll(serviceType);
+			return _resolver.TryResolveAll(serviceType);
 		}
 
 		public System.Web.Http.Dependencies.IDependencyScope BeginScope()
 		{
-			return null;
+			return this;	// null?
 		}
 
 		public void Dispose()
