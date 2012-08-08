88ac905e8cc7caf52978237bbaebd7aaa59f6ad7
Bert Willems <b.willems@premotion.nl>
2012-08-08 08:34:45 +0200

    + removed the hardcoded path to msbuild from the package script

--- package.cmd
+++ package.cmd
@@ -1,6 +1,6 @@
 .nuget\NuGet.exe update -self
 
-%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Dynamo.Ioc.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
+msbuild.exe Dynamo.Ioc.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
 .nuget\NuGet.exe pack Dynamo.Ioc.nuspec
 .nuget\NuGet.exe pack Dynamo.Ioc.Extensions.nuspec
 .nuget\NuGet.exe pack Dynamo.Ioc.Web.nuspec
