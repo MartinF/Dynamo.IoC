4b2f7573d24c26ba19f076a92339c57e1e3bd517
Martin From <from@pc.dk>
2013-05-27 20:03:18 +0200

    Moved Nuget and changed package command accordingly

--- package.cmd
+++ package.cmd
@@ -1,6 +1,6 @@
-.nuget\NuGet.exe update -self
+NuGet\NuGet.exe update -self
 
 msbuild.exe Dynamo.Ioc.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
-.nuget\NuGet.exe pack Dynamo.Ioc.nuspec
-.nuget\NuGet.exe pack Dynamo.Ioc.Extensions.nuspec
-.nuget\NuGet.exe pack Dynamo.Ioc.Web.nuspec
+NuGet\NuGet.exe pack Dynamo.Ioc.nuspec
+NuGet\NuGet.exe pack Dynamo.Ioc.Extensions.nuspec
+NuGet\NuGet.exe pack Dynamo.Ioc.Web.nuspec
